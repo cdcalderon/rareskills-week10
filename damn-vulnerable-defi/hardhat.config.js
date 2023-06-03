78b1037836c5c9f370e0659c94fff17a61d74598
carlos calderon <cdcalderon@gmail.com>
2023-06-03 18:47:23 -0500

    add rewarder DVD challenge

--- damn-vulnerable-defi/hardhat.config.js
+++ damn-vulnerable-defi/hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      { version: "0.8.18" },
+      { version: "0.8.16" },
+      { version: "0.7.6" },
+      { version: "0.6.6" },
+      { version: "0.5.14" },
+    ],
+  },
 };
